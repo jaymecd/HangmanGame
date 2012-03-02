0a3b0cddda5f7d9e1f818d97d1fa23248e456626
Nikolai Zujev <nikolai.zujev@gmail.com>
2012-03-02 01:45:04 +0100

    ApcProfilerBundle

--- app/autoload.php
+++ app/autoload.php
@@ -1,14 +1,18 @@
 <?php
 
-require __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/ApcUniversalClassLoader.php';
+//require __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/ApcUniversalClassLoader.php';
+//require __DIR__.'/../vendor/symfony/src/Symfony/Component/ClassLoader/UniversalClassLoader.php';
 
-//use Symfony\Component\ClassLoader\UniversalClassLoader;
+use Symfony\Component\ClassLoader\UniversalClassLoader;
+//use Symfony\Component\ClassLoader\ApcUniversalClassLoader;
+
+//$loader = new ApcUniversalClassLoader('rand239893_435efeEFX_');
+
+$loader = new UniversalClassLoader();
+
 use Doctrine\Common\Annotations\AnnotationRegistry;
-use Symfony\Component\ClassLoader\ApcUniversalClassLoader;
 
-$loader = new ApcUniversalClassLoader('rand239893_435efeEFX_');
 
-// $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
     'Symfony'          => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
     'Sensio'           => __DIR__.'/../vendor/bundles',
@@ -19,6 +23,7 @@ $loader->registerNamespaces(array(
     'Monolog'          => __DIR__.'/../vendor/monolog/src',
     'Assetic'          => __DIR__.'/../vendor/assetic/src',
     'Metadata'         => __DIR__.'/../vendor/metadata/src',
+    'Stealth35'        => __DIR__.'/../vendor/bundles',
     'Game'             => __DIR__.'/../src',
 ));
 $loader->registerPrefixes(array(
